50b51ac390818a7b7932290e21b5ec518a0d9bf6
Shikha0803 <vshikha0803@gmail.com>
2025-02-16 16:45:37 +0530

    this is a flipkart project repo.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.TakesScreenshot;
 
 import java.io.File;
 import java.time.Duration;
